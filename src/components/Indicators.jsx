db64da4a78972cb95e81f2e3fd0df7dc4f05c565
rydfdli <rydfadli@gmail.com>
2024-01-04 13:06:31 +0800

    modified:   src/components/Indicators.jsx 	modified:   src/pages/Details.jsx

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,27 +1,19 @@
 import React from 'react'
 
 export default function Indicators({data}) {
-    console.log(data.length)
-    if(!data) {
-        return (
-            <>
-            <h1>No data</h1>
-            </>
-        )
-    } else {
+
         return (
             <>
             <div className='flex flex-col items-center gap-3 my-4 lg:block'>
                 <p className='text-sm lg:text-base font-light opacity-25'>HP</p>
-                <progress className="progress progress-success w-56" value={data.hp} max="10000"></progress>
+                <progress className="progress progress-success w-56" value={data.hp} max="9000"></progress>
                 <p className='text-sm lg:text-base font-light opacity-25'>Movement speed</p>  
-                <progress className="progress progress-info w-56" value={data.movement_speed} max="500"></progress>
+                <progress className="progress progress-info w-56" value={data.movement_speed} max="1000"></progress>
                 <p className='text-sm lg:text-base font-light opacity-25'>Physical attack</p>
-                <progress className="progress progress-warning w-56" value={data.physical_attack} max="500"></progress>
-                <p className='text-sm lg:text-base font-light opacity-25'>Magic power</p>
-                <progress className="progress progress-secondary w-56" value={data.mana} max="2000"></progress>
+                <progress className="progress progress-warning w-56" value={data.physical_attack} max="1000"></progress>
+                <p className='text-sm lg:text-base font-light opacity-25'>Base Mana</p>
+                <progress className="progress progress-secondary w-56" value={data.mana} max="1000"></progress>
             </div>
             </>
         )
-    }
 }
