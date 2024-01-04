fce9d4e523e62ae5502eef1668a0ff84ee9ed98c
rydfdli <rydfadli@gmail.com>
2024-01-04 20:01:52 +0800

    modified:   src/components/Indicators.jsx

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -12,7 +12,7 @@ export default function Indicators({data}) {
                 <p className='text-sm lg:text-base font-light opacity-25'>Physical attack</p>
                 <progress className="progress progress-warning w-56" value={data.physical_attack} max="1000"></progress>
                 <p className='text-sm lg:text-base font-light opacity-25'>Base Mana</p>
-                <progress className="progress progress-secondary w-56" value={data.mana} max="1000"></progress>
+                <progress className="progress progress-secondary w-56" value={data.mana} max="5000"></progress>
             </div>
             </>
         )
